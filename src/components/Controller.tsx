79ee7d1ec12886bbfba8caaa2b53d43d3807de5b
maxkleinhenz <max.github@outlook.de>
2022-11-12 00:39:42 +0100

    add icons to controller

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import type { Controller as ControllerType } from "@/components/Tetris";
+import {
+  PauseIcon,
+  ArrowDownTrayIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowDownIcon,
+  ArrowPathIcon,
+} from "@heroicons/react/24/outline";
 
 type Props = {
   controller: ControllerType;
@@ -7,28 +15,62 @@ type Props = {
 
 export default function Controller({ controller }: Props): JSX.Element {
   return (
-    <div className="grid grid-rows-3 mt-6 mx-auto boder rounded-full border-solid p-3 border-gray-400 w-36 h-36">
-      <div className="flex items-center justify-center">
-        <button
-          className="border-2 border-solid border-gray-400 w-9 h-9"
-          onClick={controller.flipClockwise}
-        />
+    <div className="flex flex-1 justify-center items-center gap-10 mx-auto mt-5">
+      <div className="grid grid-rows-3 boder rounded-full border-solid p-3 border-gray-400 w-36 h-36">
+        <div className="flex items-center justify-center">
+          <button
+            className="border-2 border-solid border-gray-400 w-9 h-9"
+            title="Flip piece clockwise"
+            onClick={controller.flipClockwise}
+          >
+            <ArrowPathIcon className="text-gray-600 w-5 h-5" />
+          </button>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="border-2 border-solid border-gray-400 w-9 h-9"
+            title="Move piece left"
+            onClick={controller.moveLeft}
+          >
+            <ArrowLeftIcon className="text-gray-600 w-5 h-5" />
+          </button>
+          <button
+            className="border-2 border-solid border-gray-400 w-9 h-9"
+            title="Move piece right"
+            onClick={controller.moveRight}
+          >
+            <ArrowRightIcon className="text-gray-600 w-5 h-5" />
+          </button>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="border-2 border-solid border-gray-400 w-9 h-9"
+            title="Move piece down"
+            onClick={controller.moveDown}
+          >
+            <ArrowDownIcon className="text-gray-600 w-5 h-5" />
+          </button>
+        </div>
       </div>
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col gap-3">
         <button
-          className="border-2 border-solid border-gray-400 w-9 h-9"
-          onClick={controller.moveLeft}
-        />
+          className="border-2 border-solid border-gray-400 p-2 rounded-full"
+          title="Pause"
+        >
+          <PauseIcon
+            className="text-gray-600 w-8 h-7 p-1"
+            onClick={controller.pause}
+          />
+        </button>
         <button
-          className="border-2 border-solid border-gray-400 w-9 h-9"
-          onClick={controller.moveRight}
-        />
-      </div>
-      <div className="flex items-center justify-center">
-        <button
-          className="border-2 border-solid border-gray-400 w-9 h-9"
-          onClick={controller.moveDown}
-        />
+          className="border-2 border-solid border-gray-400 p-2 rounded-full"
+          title="Drop piece"
+        >
+          <ArrowDownTrayIcon
+            className="text-gray-600 w-8 h-7 p-1"
+            onClick={controller.hardDrop}
+          />
+        </button>
       </div>
     </div>
   );
