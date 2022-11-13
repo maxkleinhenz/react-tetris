8ea34c715ab36a55b3beae29eef214086f195c4c
maxkleinhenz <max.github@outlook.de>
2022-11-13 22:16:07 +0100

    set base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-tetris/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
